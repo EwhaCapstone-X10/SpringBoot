06c9d77acf3b6ad4e425816a808e6341df849ce5
Claire <113418806+Sunho12@users.noreply.github.com>
2025-02-14 14:14:33 +0900

    Update SecurityConfig.java

--- DriveMate/spring/src/main/java/DriveMate/spring/global/config/SecurityConfig.java
+++ DriveMate/spring/src/main/java/DriveMate/spring/global/config/SecurityConfig.java
@@ -17,11 +17,11 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/member/signup", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll() // 인증 없이 접근 가능
-                        .anyRequest().permitAll()
+                        .anyRequest().permitAll() // 모든 요청 인증 없이 허용
                 )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // JWT 인증을 사용한다면 STATELESS 적용
-                .httpBasic(Customizer.withDefaults()); // 기본 HTTP 인증 설정
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용 안 함
+                .formLogin(form -> form.disable())  // 폼 로그인 비활성화
+                .httpBasic(httpBasic -> httpBasic.disable()); // HTTP Basic 인증 비활성화
 
         return http.build();
     }
